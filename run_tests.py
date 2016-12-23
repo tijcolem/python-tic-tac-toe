baa9882f7804bd922f05e7fecd98588ff0bdec40
Administrator <tjc.coleman@gmail.com>
2016-12-23 09:27:18 -0700

    Changed print message

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
 import test.all_tests
 
-print"\n\n*******Running Open Tests (Student & Teacher)********\n\n"
 
 testSuite = test.all_tests.create_test_suite()
 text_runner = unittest.TextTestRunner(verbosity=2).run(testSuite)
+print"\n\n*******Ran Open Tests (Student & Teacher)********\n\n"
