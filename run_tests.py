6c9b4c8cf1d9f55f3426bee3f1982a1397551686
Administrator <tjc.coleman@gmail.com>
2016-12-22 18:47:37 -0700

    Added some descriptions and changed names of tests

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,7 @@
 import unittest
 import test.all_tests
+
+print"\n\n*******Running Open Tests (Student & Teacher)********\n\n"
+
 testSuite = test.all_tests.create_test_suite()
-text_runner = unittest.TextTestRunner().run(testSuite)
+text_runner = unittest.TextTestRunner(verbosity=2).run(testSuite)
